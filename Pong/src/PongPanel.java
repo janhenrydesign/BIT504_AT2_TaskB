03216696a3085472eab6273bc90a08f26534d962
Jan Henry <janetrosehenry@gmail.com>
2021-09-04 23:02:23 +1200

    Upload edited PongPanel file

--- Pong/src/PongPanel.java
+++ Pong/src/PongPanel.java
@@ -13,17 +13,18 @@ import java.awt.Font;
 
 public class PongPanel extends JPanel implements ActionListener, KeyListener {
 	
-	private final static Color BACKGROUND_COLOR = Color.BLACK;
+	private final static Color BLUE_BACKGROUND = new Color(0,0,77);
+	private final static Color BACKGROUND_COLOR = BLUE_BACKGROUND;
 	private final static int TIMER_DELAY = 5;
-	private final static int BALL_MOVEMENT_SPEED = 2;
-	private final static int POINTS_TO_WIN = 3;
-	private final static int SCORE_FONT_SIZE = 50;
+	private final static int BALL_MOVEMENT_SPEED = 3;
+	private final static int POINTS_TO_WIN = 5;
+	private final static int SCORE_FONT_SIZE = 30;
 	private final static String SCORE_FONT_FAMILY = "Sans Serif";
 	private final static int SCORE_TEXT_X = 100;
 	private final static int SCORE_TEXT_Y = 100;
-	private final static int WINNER_TEXT_X = 200;
-	private final static int WINNER_TEXT_Y = 200;
-	private final static int WINNER_FONT_SIZE = 40;
+	private final static int WINNER_TEXT_X = 150;
+	private final static int WINNER_TEXT_Y = 100;
+	private final static int WINNER_FONT_SIZE = 30;
 	private final static String WINNER_FONT_FAMILY = "Sans Serif";
 	private final static String WINNER_TEXT = "WIN!";
 	
@@ -185,14 +186,14 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 	@Override
 	public void keyPressed(KeyEvent event) {
 		if(event.getKeyCode() == KeyEvent.VK_UP) {
-			paddle2.setYVelocity(-1);
+			paddle2.setYVelocity(-3);
 		}else if (event.getKeyCode() == KeyEvent.VK_DOWN) {
-			paddle2.setYVelocity(1);	
+			paddle2.setYVelocity(3);	
 		}
 		if(event.getKeyCode() == KeyEvent.VK_W) {
-			paddle1.setYVelocity(-1);
+			paddle1.setYVelocity(-3);
 		}else if (event.getKeyCode() == KeyEvent.VK_S) {
-			paddle1.setYVelocity(1);
+			paddle1.setYVelocity(3);
 		}	
 	}
 
